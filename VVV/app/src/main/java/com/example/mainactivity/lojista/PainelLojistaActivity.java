20d670674c2b7c0d403ebdc4265c2be43bb81192
Lorran Kaíque <148860537+oLK-log@users.noreply.github.com>
2026-05-28 20:14:35 -0400

    add funcionalidade de notificaçao ao Lojista. utilizando criterio de tempo. notificacao sugestao Premium

--- VVV/app/src/main/java/com/example/mainactivity/lojista/PainelLojistaActivity.java
+++ VVV/app/src/main/java/com/example/mainactivity/lojista/PainelLojistaActivity.java
@@ -1,10 +1,19 @@
 package com.example.mainactivity.lojista;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.mainactivity.R;
@@ -54,6 +63,48 @@ public class PainelLojistaActivity extends AppCompatActivity {
                 }
                 return false;
             }
+
         });
+        criarCanalNotificacao();
+        agendarAlarme();
+    }
+
+    private void criarCanalNotificacao(){
+        //primeiro checa versao(se precisa de canal)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence nome = "Canal Premium Lojista";
+            String descricao = "Notificação sobre ofertas e upgrades do sistema VVV";
+            int importancia = NotificationManagerCompat.IMPORTANCE_DEFAULT;//faz tocar o som padrao de notificacao
+
+            NotificationChannel canal = new NotificationChannel("CANAL_PREMIUM_VVV", nome, importancia);
+            canal.setDescription(descricao);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if(notificationManager != null){
+                notificationManager.createNotificationChannel(canal);
+            }
+        }
+    }
+
+    private void agendarAlarme(){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmePremiumReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        //tempo
+        long tempoParaDisparar = SystemClock.elapsedRealtime() + (15*1000);
+        if (alarmManager != null) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+                if(alarmManager.canScheduleExactAlarms()){
+                    //tem permissao para usar alarme com tempo exato
+                    alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, tempoParaDisparar, pendingIntent);
+                }else{
+                    //n tem permissao, ent usa alarme inexato
+                    alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, tempoParaDisparar, pendingIntent);
+                }
+            }else {
+                alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, tempoParaDisparar, pendingIntent);
+            }
+        }
     }
 }
